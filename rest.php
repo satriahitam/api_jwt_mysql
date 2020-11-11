82127e830857fd13610b03f25d1b1f34a15467ec
Rizki Eka <satria12hitam@gmail.com>
2020-11-11 13:53:58 +0700

    insert database

--- rest.php
+++ rest.php
@@ -82,8 +82,43 @@
             echo $errorMsg; exit;
         }
 
-        public function returnResponse() {
+        public function returnResponse($code, $data) {
+            header("content-type: application/json");
+            $response = json_encode(['response' => ['status' => $code, "result" => $data]]);
+            echo $response; exit;
+        }
 
+        public function getAuthorizationHeader() {
+            $headers = null;
+            if (isset($_SERVER['Authorization'])) {
+                $headers = trim($_SERVER["Authorization"]);
+            }
+            else if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+                $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
+            }
+            else if (function_exists('apache_request_headers')) {
+                $requestHeaders = apache_request_headers();
+                $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+                if(isset($requestHeaders['Authorization'])) {
+                    $headers = trim($requestHeaders['Authorization']);
+                }
+            }
+            return $headers;
+        }
+
+
+        /**
+            *get access toke from header
+        **/
+
+        public function getBearerToken() {
+            $headers = $this->getAuthorizationHeader();
+            if(!empty($headers)) {
+                if(preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
+                    return $matches[1];
+                }
+            }
+            $this->throwError( ATHORIZATION_HEADER_NOT_FOUND, 'Access Token Not found.');
         }
     }
 ?>
